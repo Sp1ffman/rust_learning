e9dfb50f2d0819a82081f7a4a10480e473f84591
Adithya <adithya3012@gmail.com>
2026-04-09 19:09:02 +0530

    Squashed commit of the following:
    commit c06bac89eb74dccf42b0301d69aef7189bd9950c
    Author: Adithya <adithya3012@gmail.com>
    Date:   Thu Apr 9 19:05:47 2026 +0530
    
        feat: understanding read_line,strings,prinltn!,expect and int

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -8,7 +8,19 @@ fn main(){
     let mut guess = String::new();/*let is used to declare a variable, mut is used to make the variable mutable,
     new is used to create a new instance of a string*/
 
-    io::stdin().read_line(&mut guess).expect("Failed to read line");
+    io::stdin().read_line(&mut guess)
+    .expect("Failed to read line");
 
     println!("You guessed: {guess}");
-}
+
+    // user input integer
+    // let mut x=String::new();
+    // let mut y=String::new();
+    // println!("Please enter the first number to add:");
+    // io::stdin().read_line(&mut x).unwrap();
+    // println!("Please enter the second number to add:");
+    // io::stdin().read_line(&mut y).unwrap();    
+    // let x1:i32=x.trim().parse().unwrap();    
+    // let y1:i32=y.trim().parse().unwrap();
+    // println!("The sum of {x1} and {y} is {}",x1+y1);
+}   
